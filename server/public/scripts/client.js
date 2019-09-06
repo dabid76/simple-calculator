d44573363bfa535168abaadb08ccf63e5694d2e9
David Diaz <daemdisa@aol.com>
2019-09-06 15:51:57 -0500

    function okay

---
+++ server/public/scripts/client.js
@@ -0,0 +1,36 @@
+console.log('js');
+
+$( document ).ready( onReady );
+
+function onReady(){
+    $( '#addBtn' ).on( 'click', add );
+    $( '#minusBtn' ).on( 'click', minus );
+    $( '#timesBtn' ).on( 'click', times );
+    $( '#divideBtn' ).on( 'click', divide );
+    $( '#equalBtn' ).on( 'click', equal );
+    $( '#refreshBtn' ).on( 'click', refresh );
+}
+
+function add(){
+
+}
+
+function minus(){
+    
+}
+
+function times(){
+    
+}
+
+function divide(){
+    
+}
+
+function equal(){
+    
+}
+
+function refresh(){
+    
+}
