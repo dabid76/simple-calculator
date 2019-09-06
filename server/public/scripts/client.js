f920b4c765bef59a70ac795ddade26a6ebe40b73
David Diaz <daemdisa@aol.com>
2019-09-06 16:04:14 -0500

    all buttons are working like one

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,34 +3,54 @@ console.log('js');
 $( document ).ready( onReady );
 
 function onReady(){
-    $( '#addBtn' ).on( 'click', add );
-    $( '#minusBtn' ).on( 'click', minus );
-    $( '#timesBtn' ).on( 'click', times );
-    $( '#divideBtn' ).on( 'click', divide );
-    $( '#equalBtn' ).on( 'click', equal );
-    $( '#refreshBtn' ).on( 'click', refresh );
-}
-
-function add(){
+    $( '#addBtn' ).on( 'click', bodyInput );
+    $( '#minusBtn' ).on( 'click', bodyInput );
+    $( '#timesBtn' ).on( 'click', bodyInput );
+    $( '#divideBtn' ).on( 'click', bodyInput );
+    $( '#equalBtn' ).on( 'click', bodyInput );
+    $( '#refreshBtn' ).on( 'click', bodyInput );
+    getNumbers();
 
 }
 
-function minus(){
-    
-}
+function bodyInput(){
+    let objectToSend = {
+        first: $( '#firstN' ).val(),
+        second: $( '#secondN' ).val()
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/cal',
+        data: objectToSend
+    }).then( function( response ){
+        console.log( 'back from server with:', response );
+        getNumbers();
+    }).catch( function( err ){
+        alert( err );
+    }) // end POST
+    $( '#firstN' ).val(''),
+    $( '#secondN' ).val('')
+} // end bodyInput
 
-function times(){
-    
-}
-
-function divide(){
-    
-}
-
-function equal(){
-    
-}
-
-function refresh(){
-    
-}
+function getNumbers(){
+    // use AJAX to make a GET request
+    $.ajax({
+        type: 'GET', 
+        url: '/cal'
+    }).then( function( response ){
+        // console log out response
+        console.log( 'back from server with:', response );
+        // display things on DOM
+        let el = $('#numberOut');
+        el.empty();
+        for( let i = 0; i< response.length; i++){
+            el.append( `
+            <li>${response[i].first}</li>
+            <li>${response[i].second}</li>`
+            );
+        } // end loop
+    }).catch( function( err ){
+        // catch errors here
+        alert( err ); 
+    }) // end catch
+} // end getThings
--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require( 'body-parser' );
 const port = 5000;
 
+let cal = [];
+
 app.use( express.static( 'server/public' ));
 app.use( bodyParser.urlencoded( { extended: true } ) )
 
@@ -10,3 +12,14 @@ app.listen(port, () =>{
     console.log( 'server is up:', port );
     
 }); // end server up
+
+app.get( '/cal', ( req, res ) => {
+    console.log( 'in /cal GET' );
+    res.send( cal );
+}) // end cal
+
+app.post( '/cal', ( req, res) => {
+    console.log( 'in /cal POST', req.body );
+    cal.push( req.body );
+    res.send( 'woof' );
+}) // end things POST
