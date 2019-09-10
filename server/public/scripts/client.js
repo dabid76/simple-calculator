876742ec8f5a6dac2d58b0a13cfa59214a67c25e
David Diaz <daemdisa@aol.com>
2019-09-09 21:12:27 -0500

    took out all the logs out

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,3 @@
-console.log('js');
-
 $( document ).ready( onReady );
 
 let operator;
@@ -9,7 +7,7 @@ function onReady(){
     $( '#minusBtn' ).on( 'click', selectOp );
     $( '#timesBtn' ).on( 'click', selectOp );
     $( '#divideBtn' ).on( 'click', selectOp );
-    $( '#equalBtn' ).on( 'click', calculate );
+    $( '#equalBtn' ).on( 'click', selectOp, calculate );
     $( '#refreshBtn' ).on( 'click', clearInputs );
     $( '#deleteBtn' ).on( 'click', deleteAll );
     displayProblems();
@@ -17,19 +15,17 @@ function onReady(){
 
 function selectOp(){
     operator = $(this).data('ops');
-    console.log('symbol', operator);
 }
 
 function calculate(event) {
     event.preventDefault;
     num1 = $('.num1').val();
     num2 = $('.num2').val();
-    console.log(num1 + ' ' + num2);
 
     let sendProblem = { 
         num1: num1, 
         operator: operator, 
-        num2: num2 
+        num2: num2
     };
 
     if( num1 === '' || num2 === '') {
@@ -46,7 +42,6 @@ function calculate(event) {
         url: '/problems',
         data: sendProblem
     }).then( function( response ){
-        console.log( 'answer', response );
         displayAnswer(response);
         displayProblems();
     }).catch( function( err ){
@@ -55,56 +50,36 @@ function calculate(event) {
 } // end calculate
 
 function displayProblems() {
-    console.log( 'in displayProblems');
-    // use AJAX to make a GET request
     $.ajax({
         type: 'GET', 
         url: '/addProblems'
     }).then( function( response ){
-        console.log( 'back from server with:', response );
-        // display things on DOM
         let el = $('#numberOut');
         el.empty();
         for( let item of response){
-            el.append( '<li>' + item.num1 + ' ' + item.operator + ' ' + item.num2 + '</li>');
-        }
-        // end loop
+            el.append( `<li> ${item.num1} ${item.operator} ${item.num2}</li>`);
+        } // end loop
     }).catch( function( err ){
-        // catch errors here
         alert( err ); 
     }) // end catch
 } // end displayProblems
 
 function clearInputs() {
-    console.log('in clearInputs,');
     $('.num1').val('');
     $('.num2').val('');
-//   $.ajax({
-//     method: 'GET',
-//     url: '/clearInput'
-//   }).then(function (response) {
-//       console.log(response);
-//     $('.num1').val('');
-//     $('.num2').val('');
-//   }).catch(function (err) {
-//     alert( err );
-//   });
 } //end clearInputs
 
 function displayAnswer(response) {
-    console.log('in answer');
     let el = $('#answer');
     el.empty();
     el.append('<h3>' + response + '</h3>');
 } //end answer
 
 function  deleteAll() {
-    console.log( 'delete Btn click' );
     $.ajax({
         type: 'delete',
         url: '/addProblems'
     }).then(function (response) {
-        console.log(response);
     })
     displayProblems();
 } // end deleteAll
--- server/server.js
+++ server/server.js
@@ -8,36 +8,25 @@ let numHistory = [];
 app.use( express.static( 'server/public' ));
 app.use( bodyParser.urlencoded( { extended: true } ) )
 
-app.listen(port, () =>{
-    console.log( 'server is up:', port );
-    
+app.listen(port, () =>{    
 }); // end server up
 
 app.post( '/problems', ( req, res) => {
-    console.log( 'in /problems POST', req.body );
     numHistory.push( req.body );
     let num1 = req.body.num1;
     let num2 = req.body.num2;
     let operator = req.body.operator;
     let answer = num1 + operator + num2;
-    console.log(answer);
     answer = eval(answer);
     res.send(String( answer ));
+    res.sendStatus(201);
 }) // end of app.post
 
 app.get( '/addProblems', ( req, res ) => {
-    console.log( 'in /problems GET' );
     res.send( numHistory );
 }) // end of app.get
 
-// app.get( '/clearInput', ( req, res ) => {
-//     console.log( 'in /clearInput GET' );
-//     res.sendStatus(200);
-// }) // end app.get
-
 app.delete( '/addProblems', ( req, res ) => {
-    console.log( ' in /addProblems delete', req.body );
-    // numHistory = [];
     while(numHistory.length > 0) {
         numHistory.pop();
     }
