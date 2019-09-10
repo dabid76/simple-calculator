854eda355058cd9b194421622c1230de7ff8f532
David Diaz <daemdisa@aol.com>
2019-09-10 10:02:42 -0500

    lets try again

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express =require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 let numHistory = [];
 
 app.use( express.static( 'server/public' ));
 app.use( bodyParser.urlencoded( { extended: true } ) )
 
-app.listen(port, () =>{    
+app.listen(PORT, () =>{    
 }); // end server up
 
 app.post( '/problems', ( req, res) => {
