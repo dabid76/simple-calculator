a4afab343c238bf9b1959a43140a6ac51af937fd
David Diaz <daemdisa@aol.com>
2019-09-10 09:59:40 -0500

    delpoying

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express =require( 'express' );
 const app = express();
 const bodyParser = require( 'body-parser' );
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 let numHistory = [];
 
